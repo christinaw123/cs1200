917e65cddacf60fcff75c78ee032c33396513374
Christina Wong <christinawong@dhcp-10-250-43-78.harvard.edu>
2024-09-07 13:56:06 -0400

    1atest

--- fall2024/psets/ps0/ps0.py
+++ fall2024/psets/ps0/ps0.py
@@ -38,6 +38,18 @@ class BTvertex:
 # Runtime: O(n)
 def calculate_sizes(v):
     # Your code goes here
+
+    #Base Case for if the node is none
+    if v == None:
+        return
+    #if left subtree exists, calculate size
+    if v.left != None:
+        left_size = calculate_sizes(v.left)
+        if v.right != None:
+            right_size = calculate_sizes(v.right)
+
+    return left_size + right_size + 1
+
     pass 
 
 
